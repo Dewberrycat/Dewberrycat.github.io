e75b8d48498cedd3bab7f551204e4d2f7bacf1aa
Dewberrycat <rakkausburzum@mail.ru>
2019-11-05 23:14:11 +0300

    Add files via upload

---
+++ info3/menu.js
@@ -0,0 +1,20 @@
+var state = {
+		".div1": "none",
+		".div2": "none",
+		".div3": "none",
+		".div4": "none"
+	};
+
+	function check(cl) {
+		if (state[cl] == "none") {
+		$(".menu").css("display", "none");
+		for (var key in state) state[key] = "none";
+		$(cl).css("display", "block");
+		state[cl] = "block";
+		}
+		else {
+			$(".menu").css("display", "none");
+			for (var key in state) state[key] = "none";
+			state[cl] = "none";
+		}
+	}
